eccc4097722e0ad1c3a914c86c5b67244b8a1fac
cdore <cdore00@yahoo.ca>
2018-04-19 12:27:19 -0400

    Add files via upload

---
+++ misc/menu.js
@@ -0,0 +1,108 @@
+<!--
+
+
+var subMenus = [
+	{
+	"parent": "oMenu2",
+	"options": [
+					{
+					"caption": "Repas de la semaine",
+					"href": "https://www.traiteursimpas.com/"
+					},
+					{
+					"caption": "Nos mets",
+					"href": "https://www.traiteursimpas.com/"
+					},
+					{
+					"caption": "Questions fréquentes",
+					"href": "https://www.traiteursimpas.com/"
+					}
+				]
+	},
+	{
+	"parent": "oMenu3",
+	"options": [
+					{
+					"caption": "Buffet froid",
+					"href": "https://www.traiteursimpas.com/"
+					},
+					{
+					"caption": "Boîte à lunch",
+					"href": "https://www.traiteursimpas.com/"
+					},
+					{
+					"caption": "Cocktail dînatoire",
+					"href": "https://www.traiteursimpas.com/"
+					},
+					{
+					"caption": "Chef à domicile",
+					"href": "https://www.traiteursimpas.com/"
+					}
+				]
+	}
+]
+
+
+function initMenu(selID){
+var optColl = document.getElementsByClassName("optMenu");
+for (i = 0; i < optColl.length; i++) {
+	if (optColl[i].id == selID)
+		optColl[i].classList.add('selectedOpt');
+}
+}
+
+function showSubMenu(oOpt){
+var docBody = document.getElementsByTagName("BODY")[0];
+var parent = oOpt.parentNode;
+var subName = oOpt.id + "sub";
+var subOptions = false;
+var subElem = document.getElementById(subName);
+// Close other sub menu
+var othSub = document.getElementsByClassName("submenu");
+for (i = 0; i < othSub.length; i++) {
+	othSub[i].style.height = "0px";
+}
+if (subElem){
+	subElem.style.height = "auto";
+}else{
+	for (i = 0; i < subMenus.length; i++) {
+		if (subMenus[i].parent == oOpt.id)
+			subOptions = subMenus[i].options;
+	}
+	if (subOptions){
+		var divElem = document.createElement("div");
+		divElem.setAttribute('id', subName);
+		divElem.setAttribute('class', 'submenu');		
+		divElem.setAttribute('onmouseleave', 'outSub(this)');
+		for (i = 0; i < subOptions.length; i++) {
+			divElem.appendChild(addMenuOpt(subOptions[i]));
+		}
+		docBody.appendChild(divElem);
+		subElem = divElem;
+	}
+}
+if (subElem){
+	subElem.style.top = parent.offsetHeight + "px" ;
+	subElem.style.left = parent.offsetLeft + "px" ;
+	}
+}
+
+function addMenuOpt(attrib){
+//for (i = 0; i < optColl.length; i++) {
+var ancElem = document.createElement("a");
+ancElem.setAttribute('class', 'inputButton');
+if (attrib.href)
+	ancElem.setAttribute('href', attrib.href);
+if (attrib.target)
+	ancElem.setAttribute('target', attrib.target);
+if (attrib.caption)
+	ancElem.innerHTML = attrib.caption;
+return ancElem;
+}
+
+function outSub(oSub){
+oSub.style.height = "0px";
+}
+
+
+// -->
